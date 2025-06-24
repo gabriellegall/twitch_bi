5c352c1b72b778229f88346a8a88b9295adb5679
Gabriel LE GALL <gabrielgall@hotmail.fr>
2025-06-24 20:45:14 +0200

    added sys env

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
-subprocess.run(["python", "scripts/twitch_streams.py"], check=True)
+subprocess.run([sys.executable, "scripts/twitch_streams.py"], check=True)
 subprocess.run(["dbt", "run"], check=True)
